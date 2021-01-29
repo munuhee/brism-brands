2a8ba6ff70128473872a147bc393feede4818894
munuhee <51537638+munuhee@users.noreply.github.com>
2021-01-29 18:21:44 +0300

    reqlast

--- static/js/testimonials.js
+++ static/js/testimonials.js
@@ -1,37 +1,44 @@
-jQuery(document).ready(function($){
-  //create the slider
-  $('.cd-testimonials-wrapper').flexslider({
-    selector: ".cd-testimonials > li",
-    animation: "slide",
-    controlNav: false,
-    slideshow: false,
-    smoothHeight: true,
-    start: function(){
-      $('.cd-testimonials').children('li').css({
-        'opacity': 1,
-        'position': 'relative'
-      });
+const testimonialsContainer = document.querySelector('.testimonials-container');
+const testimonial = testimonialsContainer.querySelector('.testimonial');
+const username = testimonialsContainer.querySelector('.username');
+const role = testimonialsContainer.querySelector('.role');
+
+const testimonials = [{
+        "name": "Stephen Murichu",
+        "position": "Web developer",
+        "text": "Braams creation  made it easy and fun to choose a corporate logo for my business"
+    },
+    {
+        "name": "Crystal ,Magicals",
+        "position": "Entreprenuers",
+        "text": "This guys are amazing designers that delivered the task exactly how we needed it, do your selves a favor and use their services, you will not be disappointed by the work delivered. They will go the extra mile to make sure that you are happy. We will surely work again with them!"
+    },
+    {
+        "name": "Charles Mugo",
+        "position": "Freelancer",
+        "text": "Braams Creation is always my number one choice when i need work done perfectly according to my specification"
+    },
+    {
+        "name": "Kelly James",
+        "position": "Store Owner",
+        "text": "I am amazed by the quality of products made at Braams Creations"
+    },
+];
+let idx = 1;
+
+
+function updateTestimonial() {
+    let { name, position, photo, text } = testimonials[idx];
+
+    testimonial.innerHTML = text;
+    logo.src = photo;
+    username.innerHTML = name;
+    role.innerHTML = position;
+
+    idx++;
+    if (idx > testimonials.length - 1) {
+        idx = 0;
     }
-  });
+}
 
-  //open the testimonials modal page
-  $('.cd-see-all').on('click', function(){
-    $('.cd-testimonials-all').addClass('is-visible');
-  });
-
-  //close the testimonials modal page
-  $('.cd-testimonials-all .close-btn').on('click', function(){
-    $('.cd-testimonials-all').removeClass('is-visible');
-  });
-  $(document).keyup(function(event){
-    //check if user has pressed 'Esc'
-    if(event.which=='27'){
-      $('.cd-testimonials-all').removeClass('is-visible');	
-    }
-  });
-
-  //build the grid for the testimonials modal page
-  $('.cd-testimonials-all-wrapper').children('ul').masonry({
-    itemSelector: '.cd-testimonials-item'
-  });
-});
+setInterval(updateTestimonial, 10000);
